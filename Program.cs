35e7b01612d9bd39ac794643d94dbd4ff9f2d5e0
LucasAraujo1953 <130591551+LucasAraujo1953@users.noreply.github.com>
2023-04-26 10:46:29 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2Ex10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double p1;
+            double p2;
+            double M;
+
+            Console.Write("Entre com o valor da prova 1: ");
+            p1=double.Parse(Console.ReadLine());
+            Console.Write("Entre com o valor da prova 2: ");
+            p2=double.Parse(Console.ReadLine());
+
+            M = (p1 + 2 * p2) / 3;
+
+            if (M < 5)
+                Console.WriteLine("Aluno Reprovado");
+            else if (M >= 5)
+                Console.WriteLine("Aluno aprovado");
+        }
+    }
+}
